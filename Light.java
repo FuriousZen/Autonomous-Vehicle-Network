7be221d3cd000e0f4205bec64e6bcf648f0f47a2
FuriousZen <83908440+FuriousZen@users.noreply.github.com>
2023-01-07 01:17:33 -0500

    Add files via upload

---
+++ Light.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+
+public class Light
+{
+    boolean on;
+    Color   theColor;
+    
+    public Light(){
+        on = false;
+        theColor = Color.WHITE;
+    }
+    
+    public boolean isOn(){
+        return on;
+    }
+    
+    public void turnOn(){
+        on = true;
+    }
+    
+    public void turnOff(){
+        on = false;
+    }
+    
+    public void setColor(Color aColor){
+        theColor = aColor;
+    }
+    
+    public Color getColor(){
+        return theColor;
+    }
+    
+    public void draw(Graphics g, int x, int y){
+        if(on){
+            g.setColor(theColor);
+            g.fillRect(x, y, Board.pixelSize, Board.pixelSize);
+        }else{
+            g.setColor(new Color(10, 10, 10));
+            g.drawRect(x, y, Board.pixelSize, Board.pixelSize);
+        }
+    }
+}
